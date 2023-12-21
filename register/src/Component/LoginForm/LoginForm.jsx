ce84e65e5bd3ae0456ae273a5c0ca2a89e849981
chichkak <dneprovumar@gmail.com>
2023-12-21 17:56:07 +1300

    register done

--- register/src/Component/LoginForm/LoginForm.jsx
+++ register/src/Component/LoginForm/LoginForm.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './LoginForm.css'
-// import { FaUser, FaLock, MdAlternateEmail } from "react-icons/fa";
+import { FaUser, FaLock,  } from "react-icons/fa";
+import { MdAlternateEmail } from "react-icons/md";
+
 
 
 
@@ -12,22 +14,26 @@ const LoginForm = () => {
                 <h1>Sign up</h1>
                 <div className="input-box">
                     <input type="text" placeholder='First Name' required/>
-                    {/* <FaUser className='icon'/> */}
+                    < FaUser className='icon'/>
                 </div>
                 <div className="input-box">
                     <input type="text" placeholder='Last Name' required/>
-                    {/* <FaUser className='icon'/> */}
+                    <FaUser className='icon'/>
                 </div>
                 <div className="input-box">
                     <input type="email" placeholder='Email' required/>
-                    {/* <MdAlternateEmail className='icon'/> */}
+                    <MdAlternateEmail className='icon'/>
                 </div>
                 <div className="input-box">
                     <input type="password" placeholder='Password' required/>
-                    {/* <FaLock className='icon'/> */}
+                    <FaLock className='icon'/>
                 </div>
 
-                <div className="remember-forot"></div> 
+                <button type="submit">Sign Up</button>
+
+                <div className="login-link">
+                    {/* <p>Already have an account? <a href="Login"></a></p> */}
+                </div>
             </form>
         </div>
     )
