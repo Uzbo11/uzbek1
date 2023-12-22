ea3386af132defa0da4fb3ff071d55188c3cf152
Uzbo11 <yuldashev.ba@mail.ru>
2023-12-22 23:39:58 +0600

    changesomething

--- SecretSanta/serializers.py
+++ SecretSanta/serializers.py
@@ -1,44 +1,9 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework_simplejwt.exceptions import TokenError
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta(object):
         model = User
         fields = ['id', 'username', 'password', 'email']
-
-class LogOutSerializer(serializers.Serializer):
-    refresh = serializers.CharField(min_length=2)
-    default_error_messages = {
-        'bad_token': ('Token is invalid or expired',)}
-
-    def validate(self, attrs):
-        self.token = attrs['refresh']
-        return super().validate(attrs)
-
-    def save(self, **kwargs):
-        try:
-            RefreshToken(self.token).blacklist()
-        except TokenError:
-            self.fail('bad_token')
-
-class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField(write_only=True)
-
-class RegisterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('username', 'password', 'email')
-
-    def create(self, validated_data):
-        user = User.objects.create(
-            username=validated_data['username'],
-            email=validated_data['email']
-        )
-
-        user.set_password(validated_data['password'])
-        user.save()
-
-        return user
+        
--- SecretSanta/urls.py
+++ SecretSanta/urls.py
@@ -1,20 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
-from .views import LoginAPI, LogOutView, SignUpPage
 
 from SecretSanta import settings
 from . import views
 
 urlpatterns = [
-    path('register/', SignUpPage.as_view(), name='Sign'),
     path("admin/", admin.site.urls),
     path('signup', views.signup),
     path('login', views.login),
     path('test_token', views.test_token),
     path("users/", include("users.urls")),
-    path('login/', LoginAPI.as_view(), name='login'),
-    path('logout/', LogOutView.as_view(), name='logout')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
