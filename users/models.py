f2c19967fa8c8e0dcd856688b79f10821354f96f
Uzbo11 <yuldashev.ba@mail.ru>
2023-12-23 02:35:04 +0600

    snovanazad(

--- users/models.py
+++ users/models.py
@@ -1,12 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User, AbstractUser
+from django.contrib.auth.models import User
 
 class UserProfile(User):
     student_id = models.CharField(max_length=255)
     gender = models.BooleanField()
-
-from django.contrib.auth.models import AbstractUser
-
-class CustomUser(AbstractUser):
-    # добавьте свои дополнительные поля, если необходимо
-    pass
--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from .views import register
 
 from . import views
 
 urlpatterns = [
     path("", views.users_list),
-    path('register/', register, name='register'),
 ]
